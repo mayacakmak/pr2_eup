a503ecc3b41874b49e80f31a534a5901640e11db
Justin Huang <jstn@cs.washington.edu>
2015-05-07 17:57:02 -0700

    Remove commented out imports.

--- rapid_robot/src/rapid_robot/__init__.py
+++ rapid_robot/src/rapid_robot/__init__.py
@@ -1,7 +1,3 @@
-#from rapid_ros import init
-#from wait import wait_for_event
-#import interface
-#import navigation
 from .robot import Robot
 from .robot import RobotFactory
 from .interface import Interface

a42f8810997e56a54cf901993fb9dec26b05f80f
Maya Cakmak <mcakmak@walle.(none)>
2015-07-22 16:11:23 -0700

    team 3 indent fixes

--- pr2_eup_samples/scripts/fieldtrip/team3.py
+++ pr2_eup_samples/scripts/fieldtrip/team3.py
@@ -6,40 +6,35 @@ from pr2_eup.msg import RobotType
 ######### ADD YOUR PROGRAM BELOW ###########
 
 def main_loop(robot):
-	#display_message("Would you like me to deliver something?")
-	#wait_for_speech
-        #robot.sleep(duration=5)
+    i = 1
+    while(i==1):
+        choice = robot.ask_choice(
+            message='Would you like me to deliver something?',
+            choices=['Yes', 'No'])
+        if choice == 'Yes':
+            choice2 = robot.ask_choice(
+                message='Are you ready for me to go?',
+                choices=['Yes', 'No'])
+            if choice2 == 'Yes':
+                robot.go_to(location_name='Table1')
+                i = i + 1
+            else:
+                robot.sleep(duration=2)		
+        elif choice == 'No':
+            robot.sleep(duration=4)
+        else:
+            robot.display_message(message='I did not hear that', duration=3)
 
-	i = 1
-	while(i==1):
-		choice = robot.ask_choice(
-        	message='Would you like me to deliver something?',
-        	choices=['Yes', 'No'])
-
-    	if choice == 'Yes':
-    		choice = robot.ask_choice(
-        	message='Are you ready for me to go?',
-        	choices=['Yes', 'No'])
-        	if choice == 'Yes':
-        		robot.go_to(location_name='Table1')
-        		i = i + 1
-        	else:
-        		robot.sleep(duration=2)		
-    	if choice == 'No':
-    		robot.sleep(duration=4)
-    	else:
-    		robot.display_message(message='I did not hear that', duration=3)
-
-        while(i==2):
-		choice = robot.ask_choice(
-			message='Would you like me to go to the kitchen or the door?',
-			choices=['kitchen', 'door'])	
-		if choice == 'kitchen':
-			robot.go_to(location_name='kitchen')
-			i = i + 1
-		if choice == 'door':
-			robot.go_to(location_name='door')
-			i = i + 1
+    while(i==2):
+        choice = robot.ask_choice(
+            message='Would you like me to go to the kitchen or the door?',
+            choices=['kitchen', 'door'])	
+        if choice == 'kitchen':
+            robot.go_to(location_name='kitchen')
+            i = i + 1
+        elif choice == 'door':
+            robot.go_to(location_name='door')
+            i = i + 1
 
 #############################################
 

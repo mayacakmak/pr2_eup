e6631bf927752c627c695d82e998a3fdcdc0f0df
doitworkshop <humancenteredroboticslab@gmail.com>
2015-07-22 10:55:39 -0700

    Update dialog_sangha.py

--- pr2_eup_samples/scripts/workshop/dialog_sangha.py
+++ pr2_eup_samples/scripts/workshop/dialog_sangha.py
@@ -12,24 +12,34 @@ def main_loop(robot):
         duration=3)
 
     choice = robot.ask_choice(
-        message= 'Tell me your favorite menu, please?',
+        message= 'Tell me your favorite French Dessert, please?',
         choices=['Chocolate-Hazelnut', 'Creme Brulee',
         'Dark Roux', 'Creole Remoulade'])
 
     robot.say(text='Please wait until your order comes.')
     
     if choice == 'Chocolate-Hazelnut':
-        robot.move(x=5, y=5, theta=5, duration=4)
+        robot.move(x=.8, y=.8, theta=.8, duration=5)
         robot.say(text='Rosie the chief, cook Chocolate-Hazelnut.')
     elif choice == 'Creme Brulee':
-        robot.move(x=5, y=5, theta=5, duration=4)
+        robot.move(x=.8, y=.8, theta=.8, duration=5)
         robot.say(text='Rosie the chief, cook Creme Brulee.')
     elif choice == 'Dark Roux':
-        robot.move(x=5, y=5, theta=5, duration=4)
+        robot.move(x=.8, y=.8, theta=.8, duration=5)
         robot.say(text='Rosie the chief, cook Dark Roux.')
     elif choice == 'Creole Remoulade':
-        robot.move(x=5, y=5, theta=5, duration=4)
+        robot.move(x=.8, y=.8, theta=.8, duration=5)
         robot.say(text='Rosie the chief, cook Creole Remoulade.')
+        
+    robot.sleep(duration=20)
+    robot.move(x=0, y=0, theta=0, duration=5)
+    robot.say(text='Have you finish your meal?')
+    command=wait_for_speech()
+    
+    if command=='command 1':
+        robot.say(text='Please place your plates on me.')
+    else if command =='command 2':
+        move(x=.8, y=.8, theta=.8, duration=5)
 
 
 #############################################

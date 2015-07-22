18409bbe99891a4f857ddd199d010f3ed5c9c1e3
Maya Cakmak <mcakmak@walle.(none)>
2015-07-22 16:26:23 -0700

    tam 2 deb ug

--- pr2_eup_samples/scripts/fieldtrip/team2.py
+++ pr2_eup_samples/scripts/fieldtrip/team2.py
@@ -6,94 +6,49 @@ from pr2_eup.msg import RobotType
 ######### ADD YOUR PROGRAM BELOW ###########
 
 def main_loop(robot):
-
-    robot.sleep(duration=5)
-    #This is the definition for the choosing the color and the response for the fortune
-def color_choice(robot):
-    robot.ask.choice(
-            message = "Choose a color: red, blue, green or yellow",
-            choices = ["red","blue","green","yellow"])
-                if choice == "red":
-                    robot.say(text = "Good luck will come your way!")
-                elif choice == "blue":
-                    robot.say (text = "Happiness is the key to life, you will soon unlock your true potential ")
-                elif choice == "green":
-                    robot.say (text = "Go buy a lottery ticket")
-                else:
-                    robot.say (text = "Nope.")
-#We are going to make a fortune telling robot that hopefully spins (if we have time).
-
-#start script here
-
-choice = robot.ask_choice(
+  robot.sleep(duration=5)
+  choice = robot.ask_choice(
     message = 'Choose a number between one and four',
     choices=["1", "2", "3", "4"])
-    if choice == "1":
-        color_choice(robot)
-        #robot.ask.choice(
-            #message = "Choose a color: red, blue, green or yellow",
-            #choices = ["red","blue","green","yellow"])
-             #   if choice == "red":
-              #      robot.say("Good luck will come your way!")
-               # elif choice == "blue":
-                #    robot.say ("Happiness is the key to life, you will soon unlock your true potential ")
-                #elif choice == "green":
-                 #   robot.say ("Go buy a lottery ticket")
-                #else:
-                 #   robot.say ("Nope.")
-    if choice == "2":
-        color_choice(robot)
-       # robot.ask.choice(
-           # message = "Choose a color: red, blue, green or yellow",
-            #choices = ["red","blue","green","yellow"])
-             #   if choice == "red":
-              #      robot.say("Good luck will come your way!")
-               # elif choice == "blue":
-                #    robot.say ("Happiness is the key to life, you will soon unlock your true potential ")
-                #elif choice == "green":
-                 #   robot.say ("Go buy a lottery ticket")
-                #else:
-                 #   robot.say ("Nope.")
-    if choice == "3":
-        color_choice(robot)
-        #robot.ask.choice(
-            #message = "Choose a color: red, blue, green or yellow",
-            #choices = ["red","blue","green","yellow"])
-                #if choice == "red":
-                    #robot.say("Good luck will come your way!")
-               # elif choice == "blue":
-                   # robot.say ("Happiness is the key to life, you will soon unlock your true potential ")
-                #elif choice == "green":
-                   # robot.say ("Go buy a lottery ticket")
-                #else:
-                   # robot.say ("Nope.")
-                
-    if choice == "4":
-        color_choice(robot)
-        #robot.ask.choice(
-            #message = "Choose a color: red, blue, green or yellow",
-            #choices = ["red","blue","green","yellow"])
-                #if choice == "red":
-                    #robot.say("Good luck will come your way!")
-                #elif choice == "blue":
-                   # robot.say ("Happiness is the key to life, you will soon unlock your true potential ")
-               # elif choice == "green":
-                    #robot.say ("Go buy a lottery ticket")
-                #else:
-                    #robot.say ("Nope.")
+  if choice == "1":
+    color_choice(robot)
+  elif choice == "2":
+    color_choice(robot)
+  elif choice == "3":
+    color_choice(robot)
+  elif choice == "4":
+    color_choice(robot)
+
+#This is the definition for the choosing the color and the response for the fortune
+# def color_choice(robot):
+#     robot.ask.choice(
+#             message = "Choose a color: red, blue, green or yellow",
+#             choices = ["red","blue","green","yellow"])
+#                 if choice == "red":
+#                     robot.say(text = "Good luck will come your way!")
+#                 elif choice == "blue":
+#                     robot.say (text = "Happiness is the key to life, you will soon unlock your true potential ")
+#                 elif choice == "green":
+#                     robot.say (text = "Go buy a lottery ticket")
+#                 else:
+#                     robot.say (text = "Nope.")
+
+#We are going to make a fortune telling robot that hopefully spins (if we have time).
 
 def color_choice(robot):
-    robot.ask.choice(
-            message = "Choose a color: red, blue, green or yellow",
-            choices = ["red","blue","green","yellow"])
-                if choice == "red":
-                    robot.say("Good luck will come your way!")
-                elif choice == "blue":
-                    robot.say ("Happiness is the key to life, you will soon unlock your true potential ")
-                elif choice == "green":
-                    robot.say ("Go buy a lottery ticket")
-                else:
-                    robot.say ("Nope.")
+  choice = robot.ask_choice(
+    message = "Choose a color: red, blue, green or yellow",
+    choices = ["red","blue","green","yellow"])
+  if choice == "red":
+    robot.say(text="Good luck will come your way!")
+  elif choice == "blue":
+    robot.say(text="Happiness is the key to life, you will soon unlock your true potential ")
+  elif choice == "green":
+    robot.say(text="Go buy a lottery ticket")
+  else:
+    robot.say(text="Nope.")
+
+
 #############################################
 
 if __name__ == '__main__':

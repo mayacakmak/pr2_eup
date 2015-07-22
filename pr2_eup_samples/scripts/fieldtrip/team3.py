04e692869edcc18b26ef5a27e7a4ef8657d29dad
girlswhocoderobots <girlswhocoderobots@users.noreply.github.com>
2015-07-22 15:53:06 -0700

    Katie & Solemy

--- pr2_eup_samples/scripts/fieldtrip/team3.py
+++ pr2_eup_samples/scripts/fieldtrip/team3.py
@@ -6,9 +6,40 @@ from pr2_eup.msg import RobotType
 ######### ADD YOUR PROGRAM BELOW ###########
 
 def main_loop(robot):
+	#display_message("Would you like me to deliver something?")
+	#wait_for_speech
+    #robot.sleep(duration=5)
 
-    robot.sleep(duration=5)
+	i = 1
+	while(i==1):
+		choice = robot.ask_choice(
+        	message='Would you like me to deliver something?',
+        	choices=['Yes', 'No'])
 
+    	if choice == 'Yes':
+    		choice = robot.ask_choice(
+        	message='Are you ready for me to go?',
+        	choices=['Yes', 'No'])
+        	if choice == 'Yes'
+        		go_to(location_name='Table1')
+        		i = i + 1
+        	else:
+        		robot.sleep(duration=2)		
+    	if choice == 'No':
+    		robot.sleep(duration=4)
+    	else:
+    		display_message('I did not hear that')
+
+    while(i==2):
+    	choice = robot.ask_choice(
+        	message='Would you like me to go to the kitchen or the door?',
+        	choices=['kitchen', 'door'])	
+    		if choice == 'kitchen'
+        		go_to(location_name='kitchen')
+        		i = i + 1
+        	if choice == 'door':
+        		go_to(location_name='door')
+        		i = i + 1
 
 #############################################
 

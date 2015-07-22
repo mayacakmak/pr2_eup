8af4420432b88e505e5f0bd73c4072f0255c8ccb
girlswhocoderobots <girlswhocoderobots@users.noreply.github.com>
2015-07-22 15:37:24 -0700

    Team1 !power!

--- pr2_eup_samples/scripts/fieldtrip/team1.py
+++ pr2_eup_samples/scripts/fieldtrip/team1.py
@@ -4,7 +4,20 @@ import pr2_eup
 from pr2_eup.msg import RobotType
 
 ######### ADD YOUR PROGRAM BELOW ###########
-
+ask_choice "Should I go in a square or a circle?", yes = 
+#sleep(3)
+wait_for_speech()
+    if raw_input() = "circles"
+        say "swag"
+        for i in range (36):
+            move_forward (1)
+            turn_right(0.5)
+    else 
+        say "squares are for squares"
+        for i in range (36):
+            move_forward(1)
+            turn_right(0.5)
+        
 def main_loop(robot):
 
     robot.sleep(duration=5)

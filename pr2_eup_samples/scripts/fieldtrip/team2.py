2eea6b6a1c16ebad2d63d58bde42b1ad9bb37ee9
Maya Cakmak <mcakmak@walle.(none)>
2015-07-22 13:59:58 -0700

    adding new dummy scripts

---
+++ pr2_eup_samples/scripts/fieldtrip/team2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+import pr2_eup
+from pr2_eup.msg import RobotType
+
+######### ADD YOUR PROGRAM BELOW ###########
+
+def main_loop(robot):
+
+    robot.sleep(duration=5)
+
+
+#############################################
+
+if __name__ == '__main__':
+    rospy.init_node('pr2_eup_dialog')
+    robot = pr2_eup.RobotFactory().build(RobotType.TURTLEBOT)
+    robot.start_robot()
+    while not rospy.is_shutdown():
+        main_loop(robot)

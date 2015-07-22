0087728b63479aefbd2d7f7fae43e47e1261184d
Maya Cakmak <mcakmak@walle.(none)>
2015-07-22 14:18:21 -0700

    updated the example

--- pr2_eup_samples/scripts/workshop/example1.py
+++ pr2_eup_samples/scripts/workshop/example1.py
@@ -6,9 +6,13 @@ from pr2_eup.msg import RobotType
 #############################################
 
 def main_loop(robot):
-    robot.display_message(message='Hello, my name is Chester.', duration=3)
+    robot.display_message(
+        message='Hello, my name is Chester.',
+        duration=3)
     
-    choice = robot.ask_choice(message='What should I do?', choices=['Dance', 'Sing'])
+    choice = robot.ask_choice(
+        message='What should I do?',
+        choices=['Dance', 'Sing'])
     if choice == 'Dance':
         robot.turn_right(duration=1)
         robot.turn_left(duration=1)
@@ -19,18 +23,26 @@ def main_loop(robot):
         robot.play_sound(sound_name='sound2')
         robot.play_sound(sound_name='sound3')
 
-    robot.say(text='Now, lets speak. What should I do?')
-    command = robot.wait_for_speech()
+    robot.say(text='Now, lets speak. What should I do? Say one or two.')
+    command = robot.wait_for_speech(['one', 'two'])
+    robot.play_sound(sound_name='sound5')
+    robot.sleep(3)
+    robot.say(text='That means okay in R2D2 language.')
+    robot.sleep(3)
         
     if command == 'one':
-        robot.move(x=0.5, y=0.0, theta=0, duration=1)
+        robot.play_sound(sound_name='sound5')
+        robot.move(x=0.5, y=0.0, theta=0.7, duration=4)
     elif command == 'two':
-        robot.move(x=-0.5, y=0.0, theta=0, duration=1)
+        robot.move(x=-0.5, y=0.0, theta=-0.7, duration=4)
     else:
         robot.say(text='I could not hear you.')
 
-    robot.display_message(message='Going to drive now', duration=3)
-    robot.go_to(location_name='B')
+    robot.say(text = 'Last I will go to the door.')
+    robot.display_message(message='Initiating drive to door',
+        duration=3,
+        has_timeout=True)
+    robot.go_to(location_name='Door')
 
 #############################################
 

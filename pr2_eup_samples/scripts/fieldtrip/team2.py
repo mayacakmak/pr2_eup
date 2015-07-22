14e219c0b1e8dd4977bf51b97b62ca9a2ea20286
girlswhocoderobots <girlswhocoderobots@users.noreply.github.com>
2015-07-22 15:41:32 -0700

    Update team2.py

--- pr2_eup_samples/scripts/fieldtrip/team2.py
+++ pr2_eup_samples/scripts/fieldtrip/team2.py
@@ -8,6 +8,24 @@ from pr2_eup.msg import RobotType
 def main_loop(robot):
 
     robot.sleep(duration=5)
+#We are going to make a fortune telling robot that hopefully spins (if we have time).
+
+#start script here
+
+choice = robot.ask_choice(
+    message = 'What is your favorite color?',
+    choices=["red", "blue", "green", "yellow"])
+   
+   
+   
+    if choice == "red":
+        say("Good luck will come your way!")
+    elif choice == "blue":
+        say ("hggkhadowhlasgnv")
+    elif choice == "green":
+        say ("khahofbwocn")
+    else:
+        say ("haosdfofd")
 
 
 #############################################
@@ -17,4 +35,5 @@ if __name__ == '__main__':
     robot = pr2_eup.RobotFactory().build(RobotType.TURTLEBOT)
     robot.start_robot()
     while not rospy.is_shutdown():
-        main_loop(robot)
+        main_loop(robot) 
+    

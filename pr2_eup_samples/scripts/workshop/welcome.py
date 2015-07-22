8ae985e55e4b18b99960ee85633e3a7f5b414981
doitworkshop <humancenteredroboticslab@gmail.com>
2015-07-22 11:39:25 -0700

    Update welcome.py

--- pr2_eup_samples/scripts/workshop/welcome.py
+++ pr2_eup_samples/scripts/workshop/welcome.py
@@ -8,10 +8,10 @@ from pr2_eup.msg import RobotType
 def main_loop(robot):
 	choice = ask_choice(message='Welcome to CyberFood! Please click okay when you are ready to be seated.', choices=['Okay'])
 		if choice == 'Okay':
-			choice = ask_choice(message='How many people are in your party?', choices=['1', '2', 'Other'])
-				if choice == '1':
-					say(text='Okay. Party of one, are you ready to be seated?')
-					choice = ask_choice(message='Okay. Party of one, are you ready to be seated?', choices=['Yes', 'Not yet', 'Cancel')
+			choice = ask_choice(message='How many people are in your party?', choices=['2', '3', 'Other'])
+				if choice == '2':
+					say(text='Okay. Party of two, are you ready to be seated?')
+					choice = ask_choice(message='Okay. Party of two, are you ready to be seated?', choices=['Yes', 'Not yet', 'Cancel')
 						if choice == 'Yes':
 							say(text='please follow me')
 							display_message(message='Please follow me.', duration=8)
@@ -42,9 +42,9 @@ def main_loop(robot):
 						else:
 						    say(text='Restarting seating program, please wait.')
 							display_message(message='Restarting seating program, please wait.', duration=8)
-				else if choice == '2':
-				    say(text='Okay. Party of two, are you ready to be seated?')
-					choice = ask_choice(message='Okay. Party of two, are you ready to be seated?', choices=['Yes', 'Not yet', 'cancel')
+				else if choice == '3':
+				    say(text='Okay. Party of three, are you ready to be seated?')
+					choice = ask_choice(message='Okay. Party of three, are you ready to be seated?', choices=['Yes', 'Not yet', 'cancel')
 						if choice == 'Yes':
 							say(text='please follow me')
 							display_message(message='Please follow me.', duration=8)
@@ -74,7 +74,7 @@ def main_loop(robot):
 						    say(text='Restarting seating program, please wait.')
 							display_message(message='Restarting seating program, please wait.', duration=8)
 				else:
-				    say(text='I am sorry, but we only seat parties of one and two.')
+				    say(text='I am sorry, but we only seat parties of two and three.')
 					display_message(message='I am sorry, but we only seat parties of one and two.', duration=8)
 					say(text='Restarting seating program, please wait.')
 					display_message(message='Restarting seating program, please wait.', duration=8)

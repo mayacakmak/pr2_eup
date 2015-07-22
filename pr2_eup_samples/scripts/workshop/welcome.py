aabfdd058492b35875159352858af1ab02e8be14
doitworkshop <humancenteredroboticslab@gmail.com>
2015-07-22 10:45:42 -0700

    Update welcome.py

--- pr2_eup_samples/scripts/workshop/welcome.py
+++ pr2_eup_samples/scripts/workshop/welcome.py
@@ -6,12 +6,46 @@ from pr2_eup.msg import RobotType
 ######### ADD YOUR PROGRAM BELOW ###########
 
 def main_loop(robot):
-    robot.display_message(
-        message='Welcome to the restaurant. How many people?',
-        duration=3)
-    command = robot.wait_for_speech()
-    robot.say(text='Okay, come with me')
-    robot.move(x=0.5, y= 0.0, theta=0.5, duration=3)
+    def main_loop():
+	choice = ask_choice(message='Welcome to CyberFood! Please click okay when you are ready to be seated.', choices=['Okay'])
+		if choice == 'Okay':
+			choice = ask_choice(message='How many people are in your party?', choices=['1', '2', 'Other'])
+				if choice == '1':
+					say(text='Okay. Party of one, are you ready to be seated?')
+					choice = ask_choice(message='Okay. Party of one, are you ready to be seated?', choices=['Yes', 'Not yet', 'cancel')
+						if choice == 'Yes':
+							say(text='please follow me')
+							display_message(message='Please follow me.', duration=8)
+						else if choice == 'No'
+							say(text='Okay, I will wait. Press okay when you are ready to be seated')
+							choice = ask_choice(message=''Okay, I will wait. Press okay when you are ready to be seated', choices=['Okay'])
+								if choice == 'Okay'
+									say(text='please follow me')
+							    	display_message(message='Please follow me.', duration=8)
+						else:
+						    say(text='Restarting seating program, please wait.')
+							display_message(message='Restarting seating program, please wait.', duration=8)
+				else if choice == '2':
+				    say(text='Okay. Party of two, are you ready to be seated?')
+					choice = ask_choice(message='Okay. Party of two, are you ready to be seated?', choices=['Yes', 'Not yet', 'cancel')
+						if choice == 'Yes':
+							say(text='please follow me')
+							display_message(message='Please follow me.', duration=8)
+						else if choice == 'No'
+							say(text='Okay, I will wait. Press okay when you are ready to be seated')
+							choice = ask_choice(message=''Okay, I will wait. Press okay when you are ready to be seated', choices=['Okay'])
+								if choice == 'Okay'
+									say(text='please follow me')
+							    	display_message(message='Please follow me.', duration=8)
+						else:
+						    say(text='Restarting seating program, please wait.')
+							display_message(message='Restarting seating program, please wait.', duration=8)
+				else:
+				    say(text='I am sorry, but we only seat parties of one and two.')
+					display_message(message='I am sorry, but we only seat parties of one and two.', duration=8)
+					say(text='Restarting seating program, please wait.')
+					display_message(message='Restarting seating program, please wait.', duration=8)
+
     robot.sleep(duration=5)
 
 

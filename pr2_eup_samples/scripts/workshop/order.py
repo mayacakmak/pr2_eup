84487781a74813eb94ef361f9149cbd461950285
doitworkshop <humancenteredroboticslab@gmail.com>
2015-07-22 21:21:04 -0700

    Update order.py

--- pr2_eup_samples/scripts/workshop/order.py
+++ pr2_eup_samples/scripts/workshop/order.py
@@ -6,8 +6,54 @@ from pr2_eup.msg import RobotType
 ######### ADD YOUR PROGRAM BELOW ###########
 
 def main_loop(robot):
-
     robot.sleep(duration=5)
+    choice = ask_choice(message='How many people are ordering?', choices=['1', '2'])
+    if choice == '1':
+        choice = ask_choice(message='What would you like to eat?', choices=['sandwich', 'spaghetti', 'hamburger''])
+        if choice == 'sandwich':
+            display_message(message='OK.', duration=1)
+        else if choice == 'spaghetti':
+            display_message(message='OK.', duration=1)
+        else if choice == 'hamburger':
+            display_message(message='OK.', duration=1)
+    if choice =='2':
+        choice="ask_choice(message='Which person is ordering?', choices=['1', '2'])
+        if choice == '1':
+            choice = ask_choice(message='What would you like to eat?', choices=['sandwich', 'spaghetti', 'hamburger'])
+            if choice == 'sandwich':
+                display_message(message='OK.', duration=1)
+                choice="ask_choice(message='Which person is ordering?', choices=['2])
+                if choice == '2':
+                    choice = ask_choice(message='What would you like to eat?', choices=['sandwich', 'spaghetti', 'hamburger'])
+                    if choice = = 'sandwich':
+                        display_message(message='OK.', duration=1)
+                    else if choice = = 'spaghetti':
+                        display_message(message='OK.', duration=1)
+                    else if choice == 'hamburger':
+                        display_message(message='OK.', duration=1)
+            if choice == 'spaghetti':
+                display_message(message='OK.', duration=1)
+                choice="ask_choice(message='Which person is ordering?', choices=['2])
+                if choice == '2':
+                    choice = ask_choice(message='What would you like to eat?', choices=['sandwich', 'spaghetti', 'hamburger'])
+                    if choice = = 'sandwich':
+                        display_message(message='OK.', duration=1)
+                    else if choice = = 'spaghetti':
+                        display_message(message='OK.', duration=1)
+                    else if choice == 'hamburger':
+                        display_message(message='OK.', duration=1)
+            if choice == 'hamburger':
+                display_message(message='OK.', duration=1)
+                choice="ask_choice(message='Which person is ordering?', choices=['2])
+                if choice == '2':
+                    choice = ask_choice(message='What would you like to eat?', choices=['sandwich', 'spaghetti', 'hamburger'])
+                    if choice = = 'sandwich':
+                        display_message(message='OK.', duration=1)
+                    else if choice = = 'spaghetti':
+                        display_message(message='OK.', duration=1)
+                    else if choice == 'hamburger':
+                        display_message(message='OK.', duration=1)
+
 
 
 ############################################

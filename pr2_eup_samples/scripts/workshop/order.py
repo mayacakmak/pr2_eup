ffd42ea96d8bf077f210a4019a8cb8167e8aaf58
Maya Cakmak <mcakmak@walle.(none)>
2015-07-23 11:06:41 -0700

    fixzes

--- pr2_eup_samples/scripts/workshop/order.py
+++ pr2_eup_samples/scripts/workshop/order.py
@@ -35,17 +35,17 @@ def main_loop(robot):
                 robot_line = 'Okay, great.'
                 robot.display_message(message=robot_line)
                 robot.say(text=robot_line)
-                robot.turn_right(duration=1)
+                robot.turn_right(duration=2)
                 orders[table_choice] = 'one ' + take_order(robot)
-                robot.turn_left(duration=1)
+                robot.turn_left(duration=2)
                 if number_of_people[table_choice] == 2:
-                    robot.turn_left(duration=1)
+                    robot.turn_left(duration=2)
                 orders[table_choice] = (orders[table_choice]
                     + ', one ' + take_order(robot))
                 if number_of_people[table_choice] == 3:
-                    robot.turn_left(duration=1)
+                    robot.turn_left(duration=2)
                     orders[table_choice] = orders[table_choice] + ', one ' + take_order(robot)
-                robot.turn_right(duration=1)
+                robot.turn_right(duration=2)
                 robot_line = 'Great! You ordered ' + orders[table_choice] + '. Is that correct?'
                 robot.say(text=robot_line)
                 choice = robot.ask_choice_display_and_voice(message=robot_line,
@@ -75,20 +75,33 @@ def main_loop(robot):
         if delivered['Table1'] and delivered['Table2']:
             robot.say(text='Everything was delivered.')
             robot.sleep(duration = 30)
-        robot.say(text='I am ready to deliver orders.')
-        table_choice = robot.ask_choice(
-            message='Which table should I deliver this?',
-            choices=['Table1', 'Table2'])
+        else:
+            robot.say(text='I am ready to deliver orders.')
+            robot.sleep(duration=2)
+            if not delivered['Table1']:
+                robot.say(text='Table1 ordered: ' + orders['Table1'])
+                robot.sleep(duration=3)
+            if not delivered['Table2']:
+                robot.say(text='Table2 ordered: ' + orders['Table2'])
+                robot.sleep(duration=3)
 
-        robot.go_to(location_name=table_choice)
-        delivered[table_choice] = True
-        robot_line = 'Here is your food: ' + orders[table_choice] + '. Please take your plates and press okay.'
-        robot.say(text=robot_line)
-        robot.ask_choice(message=robot_line, choices=['OK'])
-        robot_line = 'Enjoy your food!'
-        robot.say(text=robot_line)
-        robot.display_message(message=robot_line)
-        robot.go_to(location_name='Kitchen')
+            table_choice = robot.ask_choice(
+                message='Which table should I deliver this?',
+                choices=['Table1', 'Table2'])
+
+            if not delivered[table_choice]:
+                robot.go_to(location_name=table_choice)
+                robot_line = 'Here is your food: ' + orders[table_choice] + '. Please take your plates and press okay.'
+                robot.say(text=robot_line)
+                robot.ask_choice(message=robot_line, choices=['OK'])
+                robot_line = 'Enjoy your food!'
+                robot.say(text=robot_line)
+                robot.display_message(message=robot_line)
+                delivered[table_choice] = True
+                robot.go_to(location_name='Kitchen')
+            else:
+                robot.say(text='I have already delivered to ' + table_choice)
+                robot.sleep(duration=2)
 
 
 def take_order(robot):
